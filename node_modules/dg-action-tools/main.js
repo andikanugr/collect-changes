3b6248684826a8d8491f5d9e27c2b0743fce04fb
Andika Nugraha <andika.nugraha@tokopedia.com>
2022-08-26 22:21:37 +0700

    fix action

---
+++ node_modules/dg-action-tools/main.js
@@ -0,0 +1,5 @@
+const { Member } = require("./member/member.js")
+const { Slack } = require("./slack/slack.js")
+const { Sheet } = require("./sheet/speadsheet.js")
+
+module.exports={Member, Slack, Sheet}
